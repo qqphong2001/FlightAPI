8f5d252e2f54fb749a84613df475aeaf95bbaf5a
qqphong2001 <74895683+qqphong2001@users.noreply.github.com>
2023-04-04 19:05:19 +0700

    a

---
+++ dotnetAPI/Migrations/20230404120228_add-v1.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace dotnetAPI.Migrations
+{
+    public partial class addv1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
